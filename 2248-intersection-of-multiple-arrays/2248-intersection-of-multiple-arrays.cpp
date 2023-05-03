0d26fdcce72c5b262f1b0625f035825a27fada6f
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-05-03 16:05:02 +0900

    Time: 17 ms (36.19%), Space: 12.2 MB (84.42%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -2,45 +2,24 @@ class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums)
     {
-        size_t N = nums.size();
-        if(N == 1)
-        {
-            sort(nums[0].begin(), nums[0].end());
-            return nums.front();
-        }
+        vector<int> Count(1001);
         
-        size_t MaxIndex = 0;
-        size_t MaxSize = 0;
-        for(size_t i = 0; i < N; i++)
+        for(vector<int>& Row : nums)
         {
-            sort(nums[i].begin(), nums[i].end());
-            
-            if(MaxSize < nums[i].size())
+            for(int& Col : Row)
             {
-                MaxIndex = i;
-                MaxSize = nums[i].size();
+                Count[Col]++;
             }
         }
-        swap(nums[0], nums[MaxIndex]);
         
+        int N = static_cast<int>(nums.size());
         vector<int> Result;
         
-        for(size_t Col = 0; Col < MaxSize; Col++)
+        for(int i = 1; i < 1001; i++)
         {
-            int Target = nums[0][Col];
-            bool Flag = true;
-            
-            for(size_t Row = 1; Row < N; Row++)
+            if(Count[i] == N)
             {
-                if(!binary_search(nums[Row].begin(), nums[Row].end(), Target))
-                {
-                    Flag = false;
-                    break;
-                }
-            }
-            if(Flag)
-            {
-                Result.push_back(Target);
+                Result.push_back(i);
             }
         }
         
