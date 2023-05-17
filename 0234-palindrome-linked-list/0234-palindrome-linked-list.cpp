ce6de89370a0a547e4fdf4aaf498f0cf9c7df4c3
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-05-17 16:11:50 +0900

    Time: 200 ms (96.23%), Space: 110.3 MB (99.24%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,17 +1,46 @@
-int init = [] {
-    ios_base::sync_with_stdio(false);  cin.tie(nullptr);
-    ofstream out("user.out");
-    for (string s; getline(cin, s);)
-        out<<(equal(s.begin()+1, s.begin()+s.size()/2, s.rbegin() + 1) ? "true\n" : "false\n");
-    out.flush();
-    exit(0);
-    
-    return 0;
-}();
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        return 0;
+    bool isPalindrome(ListNode* head)
+    {
+        ListNode* Slow = head;
+        ListNode* Fast = head;
+        ListNode* Prev = nullptr;
+        
+        while(Fast && Fast->next)
+        {
+            Fast = Fast->next->next;
+            
+            ListNode* Temp = Slow->next;
+            Slow->next = Prev;
+            Prev = Slow;
+            Slow = Temp;
+        }
+        
+        if(Fast)
+        {
+            Slow = Slow->next;
+        }
+        
+        while(Slow)
+        {
+            if(Prev->val != Slow->val)
+            {
+                return false;
+            }
+            Prev = Prev->next;
+            Slow = Slow->next;
+        }
+        
+        return true;
     }
 };
