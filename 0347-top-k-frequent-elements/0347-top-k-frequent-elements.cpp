8f9a7ffa7a50536e0369b1dfcb97d1b2189f3da9
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-05-22 13:53:46 +0900

    Time: 20 ms (43.48%), Space: 13.7 MB (42.78%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    typedef pair<int, int> Element;
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        map<int, int> Freq;
+        
+        for(int& Num : nums)
+        {
+            Freq[Num]++;
+        }
+        
+        auto Compare = [](Element E1, Element E2)
+        {
+            return E1.second < E2.second;
+        };
+        priority_queue<Element, vector<Element>, bool(*)(Element, Element)> MaxHeap(Compare);
+        
+        for(pair<const int, int>& Num : Freq)
+        {
+            MaxHeap.push({Num.first, Num.second});
+        }
+        
+        vector<int> Answer(k);
+        
+        for(int i = 0; i < k; i++)
+        {
+            Answer[i] = MaxHeap.top().first;
+            MaxHeap.pop();
+        }
+        
+        return Answer;
+    }
+};
