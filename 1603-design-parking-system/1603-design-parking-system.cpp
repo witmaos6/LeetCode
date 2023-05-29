526a21e2c3195f3b4d4f7a004dc39762be069f32
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-05-29 13:53:19 +0900

    Time: 72 ms (17.68%), Space: 32.9 MB (88.19%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -4,12 +4,8 @@ class ParkingSystem {
 public:
     ParkingSystem(int big, int medium, int small)
     {
-        ParkSize.resize(4);
+        ParkSize = {0, big, medium, small};
         ParkIndex.resize(4);
-        
-        ParkSize[1] = big;
-        ParkSize[2] = medium;
-        ParkSize[3] = small;
     }
     
     bool addCar(int carType)
