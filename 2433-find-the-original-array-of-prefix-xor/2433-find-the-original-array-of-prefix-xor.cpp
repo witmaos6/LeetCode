82c9651689048ea706d66916311375a38e96442d
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-06-08 15:36:17 +0900

    Time: 152 ms (10.02%), Space: 75.9 MB (96.53%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -4,14 +4,13 @@ public:
     {
         const size_t N = pref.size();
         
-        vector<int> Origin(N);
-        Origin[0] = pref[0];
-        
+        int Prev = 0;
         for(size_t i = 1; i < N; i++)
         {
-            Origin[i] = pref[i - 1] ^ pref[i];
+            Prev ^= pref[i - 1];
+            pref[i] ^= Prev;
         }
         
-        return Origin;
+        return pref;
     }
 };
