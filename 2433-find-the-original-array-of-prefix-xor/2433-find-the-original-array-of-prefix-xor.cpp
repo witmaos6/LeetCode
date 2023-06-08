112e9629b1f9f16802fec5604e9e6678d242c659
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-06-08 15:33:57 +0900

    Time: 161 ms (5.52%), Space: 76 MB (83.75%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -9,28 +9,7 @@ public:
         
         for(size_t i = 1; i < N; i++)
         {
-            Origin[i] = GetOrigin(pref[i - 1], pref[i]);
-        }
-        
-        return Origin;
-    }
-    
-private:
-    int GetOrigin(int Value, int Result)
-    {
-        int Origin = 0;
-        int Coefficient = 1;
-        
-        while(Value || Result)
-        {
-            if((Result & 1) != (Value & 1))
-            {
-                Origin += Coefficient;
-            }
-            Coefficient <<= 1;
-            
-            Value >>= 1;
-            Result >>= 1;
+            Origin[i] = pref[i - 1] ^ pref[i];
         }
         
         return Origin;
