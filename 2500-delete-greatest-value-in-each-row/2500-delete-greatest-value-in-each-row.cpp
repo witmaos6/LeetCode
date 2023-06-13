38235f0bfd5733b048a61fd20dd49c523ce5a5f4
geonhee <62473633+witmaos6@users.noreply.github.com>
2023-06-13 14:14:28 +0900

    Time: 11 ms (93.30%), Space: 9.4 MB (89.02%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -5,26 +5,20 @@ public:
         const int RowSize = static_cast<int>(grid.size());
         const int ColSize = static_cast<int>(grid[0].size());
         
-        vector<priority_queue<int, vector<int>>> MaxHeap(RowSize);
-        
         for(int Row = 0; Row < RowSize; Row++)
         {
-            for(int& Num : grid[Row])
-            {
-                MaxHeap[Row].push(Num);
-            }
+            sort(grid[Row].begin(), grid[Row].end());
         }
         
         int Answer = 0;
         
-        for(int Col = 0; Col < ColSize; Col++)
+        for(int Col = ColSize - 1; Col >= 0; Col--)
         {
             int MaxValue = 0;
             
             for(int Row = 0; Row < RowSize; Row++)
             {
-                MaxValue = max(MaxValue, MaxHeap[Row].top());
-                MaxHeap[Row].pop();
+                MaxValue = max(MaxValue, grid[Row][Col]);
             }
             
             Answer += MaxValue;
