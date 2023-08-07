f7bfe5e9a05eacd2003d711dd34d6f5f21d3fe6c
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-08-07 13:43:18 +0900

    Time: 7 ms (27.24%), Space: 9.4 MB (90.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,48 +1,31 @@
 class Solution {
-    int RowSize = 0;
-    int ColSize = 0;
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target)
     {
-        RowSize = static_cast<int>(matrix.size());
-        ColSize = static_cast<int>(matrix[0].size());
+        int RowSize = static_cast<int>(matrix.size());
+        int ColSize = static_cast<int>(matrix[0].size());
         
-        int BoundaryRow = GetBoundaryRow(matrix, target);
+        int Left = 0, Right = RowSize * ColSize - 1;
         
-        if(BoundaryRow >= RowSize)
-        {
-            return false;
-        }
-        
-        return binary_search(matrix[BoundaryRow].begin(), matrix[BoundaryRow].end(), target);
-    }
-    
-private:
-    int GetBoundaryRow(vector<vector<int>>& Matrix, int Target)
-    {
-        int Left = 0, Right = RowSize;
-        
-        while(Left < Right)
+        while(Left <= Right)
         {
             int Middle = (Right + Left) / 2;
+            int MidValue = matrix[Middle / ColSize][Middle % ColSize];
             
-            if(Matrix[Middle].front() <= Target)
+            if(MidValue == target)
             {
-                if(Matrix[Middle].back() >= Target)
-                {
-                    return Middle;
-                }
-                else
-                {
-                    Left = Middle + 1;
-                }
+                return true;
             }
-            else
+            else if(MidValue > target)
             {
                 Right = Middle - 1;
             }
+            else
+            {
+                Left = Middle + 1;
+            }
         }
         
-        return Left;
+        return false;
     }
 };
