b685cdc5f6eb23aef403282962cdbfe3d187be11
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-08-09 14:32:26 +0900

    Time: 135 ms (90.56%), Space: 81.1 MB (38.62%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,54 @@
+class Solution {
+    int N = 0;
+public:
+    int minimizeMax(vector<int>& nums, int p)
+    {
+        N = static_cast<int>(nums.size());
+        if(N == 1 || p == 0)
+        {
+            return 0;
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        int Low = 0, High = nums[N - 1] - nums[0];
+        
+        while(Low < High)
+        {
+            int Middle = (Low + High) / 2;
+            
+            if(PossiblePair(nums, Middle, p))
+            {
+                High = Middle;
+            }
+            else
+            {
+                Low = Middle + 1;
+            }
+        }
+        
+        return Low;
+    }
+    
+private:
+    bool PossiblePair(vector<int>& Nums, int Middle, int Pair)
+    {
+        int Sum = 0;
+        
+        for(int i = 0; i < N - 1; i++)
+        {
+            if(Nums[i + 1] - Nums[i] <= Middle)
+            {
+                Sum++;
+                i++;
+            }
+            
+            if(Sum >= Pair)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
