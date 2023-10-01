ecdfee648788b2bba6379309b2e80781abf008a3
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-10-01 14:45:22 +0900

    Time: 17 ms (46.37%), Space: 10 MB (59.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     string reverseWords(string s)
     {
-        s += ' ';
         int N = static_cast<int>(s.size());
         int Begin = 0;
         
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i <= N; i++)
         {
-            if(s[i] == ' ')
+            if(s[i] == ' ' || i == N)
             {
                 reverse(s.begin() + Begin, s.begin() + i);
                 Begin = i + 1;
             }
         }
         
-        s.pop_back();
         return s;
     }
 };
