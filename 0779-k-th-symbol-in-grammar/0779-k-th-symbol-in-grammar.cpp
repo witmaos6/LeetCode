48d92c6e7e6cc62b2de7a96925c94a1dbfee6ba0
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-10-25 10:34:01 +0900

    Time: 2 ms (51.37%), Space: 6.5 MB (12.22%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+        
+        int RowLength = 1 << (n - 2);
+        if (k <= RowLength)
+        {
+            return kthGrammar(n - 1, k);
+        }
+        return 1 - kthGrammar(n - 1, k - RowLength);
+    }
+};
