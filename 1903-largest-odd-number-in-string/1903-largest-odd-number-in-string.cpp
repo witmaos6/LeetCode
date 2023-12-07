6f9d5e897a39513f8a7f4cb7c24b8d500f1767eb
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-12-07 11:45:21 +0900

    Time: 23 ms (91.89%), Space: 15.2 MB (58.43%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,16 +4,14 @@ public:
     {
         const int N = static_cast<int>(num.size());
         
-        string Result;
         for(int i = N - 1; i >= 0; i--)
         {
             if(num[i] & 1)
             {
-                Result = num.substr(0, i + 1);
-                break;
+                return num.substr(0, i + 1);
             }
         }
         
-        return Result;
+        return "";
     }
 };
