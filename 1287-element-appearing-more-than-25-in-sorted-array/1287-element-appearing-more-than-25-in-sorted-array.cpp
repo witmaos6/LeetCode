e6ccefe8640423447d0b7705136d5c706eadb6d5
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-12-11 10:30:35 +0900

    Time: 8 ms (78.76%), Space: 12.8 MB (72.53%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -3,17 +3,30 @@ public:
     int findSpecialInteger(vector<int>& arr)
     {
         const int N = static_cast<int>(arr.size());
-        const int More25 = N / 4;
-        
-        vector<int> Table(100001);
-        for(int Num : arr)
+        if(N == 1)
         {
-            Table[Num]++;
-            if(Table[Num] > More25)
+            return arr[0];
+        }
+        const int More25 = N / 4;
+        int Freq = 1;
+        
+        for(int i = 0; i < N - 1; i++)
+        {
+            if(arr[i] == arr[i + 1])
             {
-                return Num;
+                Freq++;
+                
+                if(Freq > More25)
+                {
+                    return arr[i];
+                }
+            }
+            else
+            {
+                Freq = 1;
             }
         }
+        
         return -1;
     }
 };
