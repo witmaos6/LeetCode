94d1754a1b1af22a9a568c10e6c72b64a923d4a3
WindOn <62473633+witmaos6@users.noreply.github.com>
2023-12-14 13:55:03 +0900

    Time: 200 ms (48.70%), Space: 116.5 MB (89.58%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,49 +1,30 @@
 class Solution {
-    int RowSize = 0;
-    int ColSize = 0;
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid)
     {
-        RowSize = static_cast<int>(grid.size());
-        ColSize = static_cast<int>(grid[0].size());
+        const int RowSize = static_cast<int>(grid.size());
+        const int ColSize = static_cast<int>(grid[0].size());
         
-        vector<vector<pair<int, int>>> Memo(2);
-        for(int Row = 0; Row < RowSize; Row++)
-        {
-            int Count = 0;
-            for(int Col = 0; Col < ColSize; Col++)
-            {
-                if(grid[Row][Col] == 1)
-                {
-                    Count++;
-                }
-            }
-            Memo[0].push_back({Count, ColSize - Count});
-        }
-        for(int Col = 0; Col < ColSize; Col++)
-        {
-            int Count = 0;
-            for(int Row = 0; Row < RowSize; Row++)
-            {
-                if(grid[Row][Col] == 1)
-                {
-                    Count++;
-                }
-            }
-            Memo[1].push_back({Count, RowSize - Count});
-        }
-        
-        vector<vector<int>> Diff(RowSize, vector<int>(ColSize));
+        vector<int> RowOnes(RowSize);
+        vector<int> ColOnes(ColSize);
         
         for(int Row = 0; Row < RowSize; Row++)
         {
             for(int Col = 0; Col < ColSize; Col++)
             {
-                int RowValue = Memo[0][Row].first - Memo[0][Row].second;
-                int ColValue = Memo[1][Col].first - Memo[1][Col].second;
-                Diff[Row][Col] = RowValue + ColValue;
+                RowOnes[Row] += grid[Row][Col];
+                ColOnes[Col] += grid[Row][Col];
             }
         }
-        return Diff;
+        
+        for(int Row = 0; Row < RowSize; Row++)
+        {
+            for(int Col = 0; Col < ColSize; Col++)
+            {
+                grid[Row][Col] = 2 * (RowOnes[Row] + ColOnes[Col]) - RowSize - ColSize;
+            }
+        }
+        
+        return grid;
     }
 };
