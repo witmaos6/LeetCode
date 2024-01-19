acfc844ab53c7a84b63027e56867811af5860f9c
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-01-19 12:43:56 +0900

    Time: 7 ms (97.86%), Space: 11 MB (35.74%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix)
     {
-        const int RowSize = static_cast<int>(matrix.size());
-        const int ColSize = static_cast<int>(matrix[0].size());
+        const int N = static_cast<int>(matrix.size());
         
-        vector<vector<int>> Memo(RowSize, vector<int>(ColSize + 2, INT_MAX));
-        for(int Col = 0; Col < ColSize; Col++)
+        vector<vector<int>> Memo(N, vector<int>(N + 2, INT_MAX));
+        for(int Col = 0; Col < N; Col++)
         {
             Memo[0][Col + 1] = matrix[0][Col];
         }
         
-        for(int Row = 1; Row < RowSize; Row++)
+        for(int Row = 1; Row < N; Row++)
         {
-            for(int Col = 1; Col <= ColSize; Col++)
+            for(int Col = 1; Col <= N; Col++)
             {
                 
                 int MinPrevRow = min(Memo[Row - 1][Col - 1], min(Memo[Row - 1][Col], Memo[Row - 1][Col + 1]));
@@ -22,9 +21,9 @@ public:
         }
         
         int MinSum = INT_MAX;
-        for(int i = 1; i < ColSize + 1; i++)
+        for(int i = 1; i <= N; i++)
         {
-            MinSum = min(MinSum, Memo[RowSize - 1][i]);
+            MinSum = min(MinSum, Memo[N - 1][i]);
         }
         return MinSum;
     }
