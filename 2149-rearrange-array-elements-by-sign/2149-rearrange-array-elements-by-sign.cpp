19b68423a7c1e4a693c3492400d91eb65fbdf160
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-02-14 16:48:23 +0900

    Time: 144 ms (79.37%), Space: 126.1 MB (65.68%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,28 +3,22 @@ public:
     vector<int> rearrangeArray(vector<int>& nums)
     {
         const int N = static_cast<int>(nums.size());
-        vector<int> Positive;
-        vector<int> Negative;
-        Positive.reserve(N / 2);
-        Negative.reserve(N / 2);
-        
-        for(int Num : nums)
-        {
-            if(Num > 0)
-            {
-                Positive.push_back(Num);
-            }
-            else if(Num < 0)
-            {
-                Negative.push_back(Num);
-            }
-        }
+        int PositiveIndex = 0;
+        int NegativeIndex = 1;
         
         vector<int> Result(N);
-        for(int i = 0; i < N; i += 2)
+        for(int i = 0; i < N; i++)
         {
-            Result[i] = Positive[i / 2];
-            Result[i + 1] = Negative[i / 2];
+            if(nums[i] > 0)
+            {
+                Result[PositiveIndex] = nums[i];
+                PositiveIndex += 2;
+            }
+            else if(nums[i] < 0)
+            {
+                Result[NegativeIndex] = nums[i];
+                NegativeIndex += 2;
+            }
         }
         
         return Result;
