c349f81468b5ff39dde0809acf0cf979584b7270
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-02-26 12:48:23 +0900

    Time: 5 ms (24.89%), Space: 11.6 MB (25.40%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,62 +13,19 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q)
     {
-        if(p == nullptr && q == nullptr)
+        if(p == nullptr && q == nullptr) 
         {
             return true;
         }
-        else if(p == nullptr || q == nullptr)
+        else if(p == nullptr || q == nullptr) 
         {
             return false;
         }
-        queue<TreeNode*> BFS1;
-        queue<TreeNode*> BFS2;
-        BFS1.push(p);
-        BFS2.push(q);
-        
-        while(!BFS1.empty() && !BFS2.empty())
+        else if(p->val != q->val)
         {
-            TreeNode* Node1 = BFS1.front();
-            BFS1.pop();
-            TreeNode* Node2 = BFS2.front();
-            BFS2.pop();
+            return false;
+        }
             
-            if(Node1->val != Node2->val)
-            {
-                return false;
-            }
-            if(Node1->left && Node2->left)
-            {
-                BFS1.push(Node1->left);
-                BFS2.push(Node2->left);
-            }
-            else if(Node1->left == nullptr && Node2->left == nullptr)
-            {
-                
-            }
-            else
-            {
-                return false;
-            }
-            if(Node1->right && Node2->right)
-            {
-                BFS1.push(Node1->right);
-                BFS2.push(Node2->right);
-            }
-            else if(Node1->right == nullptr && Node2->right == nullptr)
-            {
-                
-            }
-            else
-            {
-                return false;
-            }
-        }
-        
-        if(!BFS1.empty() || !BFS2.empty())
-        {
-            return false;
-        }
-        return true;
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     } 
 };
