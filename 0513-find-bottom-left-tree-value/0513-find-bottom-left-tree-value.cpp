ef0dc071d5ba0f84195ce940832fd755f1ed931a
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-02-28 11:33:06 +0900

    Time: 11 ms (42.78%), Space: 20.2 MB (63.53%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root)
+    {
+        queue<TreeNode*> BFS;
+        BFS.push(root);
+        int LeftMost = 0;
+        
+        while(!BFS.empty())
+        {
+            size_t Range = BFS.size();
+            for(size_t i = 0; i < Range; i++)
+            {
+                TreeNode* Node = BFS.front();
+                BFS.pop();
+                if(i == 0)
+                {
+                    LeftMost = Node->val;
+                }
+
+                if(Node->left)
+                {
+                    BFS.push(Node->left);
+                }
+                if(Node->right)
+                {
+                    BFS.push(Node->right);
+                }
+            }
+        }
+        
+        return LeftMost;
+    }
+};
