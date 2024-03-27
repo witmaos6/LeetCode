2e5c3c77baa26dc6083e60571d51fcc162e34d1b
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-03-27 10:57:39 +0900

    Time: 65 ms (26.69%), Space: 63.6 MB (90.38%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k)
+    {
+        const int N = static_cast<int>(nums.size());
+        int Left = 0;
+        int Right = 0;
+        int Product = 1;
+        int Count = 0;
+        
+        while(Right < N)
+        {
+            Product *= nums[Right];
+            Right++;
+            
+            while(Left < Right && Product >= k)
+            {
+                Product /= nums[Left];
+                Left++;
+            }
+            
+            Count += (Right - Left);
+        }
+        
+        return Count;
+    }
+};
