4420b13f9e4776e42763dc4e36ab5ec02d40fa24
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-03-27 11:03:30 +0900

    Time: 47 ms (92.68%), Space: 63.6 MB (57.48%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -7,22 +7,19 @@ public:
         
         const int N = static_cast<int>(nums.size());
         int Left = 0;
-        int Right = 0;
         int Product = 1;
         int Count = 0;
         
-        while(Right < N)
+        for(int Right = 0; Right < N; Right++)
         {
             Product *= nums[Right];
-            Right++;
-            
-            while(Left < Right && Product >= k)
+            while(Left < Right + 1 && Product >= k)
             {
                 Product /= nums[Left];
                 Left++;
             }
             
-            Count += (Right - Left);
+            Count += (Right - Left) + 1;
         }
         
         return Count;
