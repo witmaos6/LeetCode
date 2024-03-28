2da1b9102120c871bd34ec471a524da6a4f4e2f6
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-03-28 13:40:51 +0900

    Time: 221 ms (42.69%), Space: 148.4 MB (86.57%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,26 +4,20 @@ public:
     {
         const int N = static_cast<int>(nums.size());
         unordered_map<int, int> Freq;
-        int MaxFreq = 0;
-        int CurrGood = 0;
         int MaxGood = 0;
         int Left = 0;
         
         for(int i = 0; i < N; i++)
         {
             Freq[nums[i]]++;
-            MaxFreq = max(MaxFreq, Freq[nums[i]]);
             
-            while(MaxFreq > k && Left < i)
+            while(Freq[nums[i]] > k)
             {
                 Freq[nums[Left]]--;
                 Left++;
-                MaxFreq = Freq[nums[i]];
-                CurrGood--;
             }
-            CurrGood++;
             
-            MaxGood = max(MaxGood, CurrGood);
+            MaxGood = max(MaxGood, i - Left + 1);
         }
         
         return MaxGood;
