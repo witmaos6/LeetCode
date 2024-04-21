79ba071cb2ef9b658c6ab31a3fc8366234d14f8a
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-04-21 16:25:18 +0900

    Time: 281 ms (91.05%), Space: 123.8 MB (89.14%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,38 +1,51 @@
 class Solution {
+    vector<int> Union;
+    vector<int> Size;
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination)
     {
-        if(source == destination)
-            return true;
+        Union.resize(n);
+        Size.resize(n, 1);
         
-        unordered_map<int, vector<int>> Graph;
-        for(vector<int>& Edge : edges)
+        for(int i = 0; i < n; i++)
         {
-            Graph[Edge[0]].push_back(Edge[1]);
-            Graph[Edge[1]].push_back(Edge[0]);
+            Union[i] = i;
         }
-        
-        vector<bool> Visited(n);
-        queue<int> BFS;
-        BFS.push(source);
-        while(!BFS.empty())
-        {
-            int CurrNode = BFS.front();
-            BFS.pop();
-            if(Visited[CurrNode])
-            {
-                continue;
-            }
-            Visited[CurrNode] = true;
-            
-            for(int NextNode : Graph[CurrNode])
-            {
-                if(NextNode == destination)
-                    return true;
 
-                BFS.push(NextNode);
-            }
+        for(const vector<int>& Edge : edges)
+        {
+            UnionFind(Edge[0], Edge[1]);
+            
+            if(Union[source] == Union[destination])
+                return true;
         }
-        return false;
+        
+        return Find(source) == Find(destination);
+    }    
+private:
+    void UnionFind(int U, int V)
+    {
+        U = Find(U);
+        V = Find(V);
+        
+        if(U == V)
+            return;
+        
+        if(Size[U] > Size[V])
+        {
+            swap(U, V);
+        }
+        
+        Union[U] = V;
+        Size[V] += Size[U];
+    }
+    
+    int Find(int U)
+    {
+        while(U != Union[U])
+        {
+            U = Union[U];
+        }
+        return U;
     }
 };
