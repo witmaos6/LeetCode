1a47f0f1f125219a8e163feec50cd5d0d94c7eba
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-05-28 13:19:37 +0900

    Time: 3 ms (97.07%), Space: 8.8 MB (37.19%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -3,25 +3,19 @@ public:
     int equalSubstring(string s, string t, int maxCost)
     {
         const int N = static_cast<int>(s.size());
-        vector<int> Costs(N);
-        
-        for(int i = 0; i < N; i++)
-        {
-            int Cost = abs(s[i] - t[i]);
-            Costs[i] = Cost;
-        }
-        
         int MaxLength = 0;
         int CurrCost = 0;
         int Left = 0;
         
         for(int Right = 0; Right < N; Right++)
         {
-            CurrCost += Costs[Right];
+            int Cost = abs(s[Right] - t[Right]);
+            CurrCost += Cost;
             
             while(CurrCost > maxCost)
             {
-                CurrCost -= Costs[Left];
+                int LeftCost = abs(s[Left] - t[Left]);
+                CurrCost -= LeftCost;
                 Left++;
             }
             
