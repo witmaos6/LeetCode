80007211f804b29603afb0e96cf9ede8c422d35b
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-10-03 14:28:21 +0900

    Time: 133 ms (64.27%), Space: 70.3 MB (94.34%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p)
+    {
+        long TotalSum = 0;
+        for (int Num : nums)
+        {
+            TotalSum += Num;
+        }
+
+        int Remain = TotalSum % p;
+        if (Remain == 0)
+            return 0;
+
+        unordered_map<int, int> PrefixMod;
+        PrefixMod[0] = -1;
+        long PrefixSum = 0;
+        const int N = static_cast<int>(nums.size());
+        int MinLength = N;
+
+        for (int i = 0; i < N; ++i)
+        {
+            PrefixSum += nums[i];
+            int CurrentMod = PrefixSum % p;
+            int TargetMod = (CurrentMod - Remain + p) % p;
+
+            if (PrefixMod.find(TargetMod) != PrefixMod.end())
+            {
+                MinLength = min(MinLength, i - PrefixMod[TargetMod]);
+            }
+
+            PrefixMod[CurrentMod] = i;
+        }
+
+        return MinLength == N ? -1 : MinLength;
+    }
+};
