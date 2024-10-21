15b1119bac8c99f813e20a90a5982c212d72ed03
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-10-21 12:53:00 +0900

    Time: 210 ms (75.44%), Space: 47.8 MB (66.67%) - LeetHub

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    unordered_set<string> Table;
+    int N = 0;
+    int MaxSplit = 0;
+public:
+    int maxUniqueSplit(string s)
+    {
+        N = static_cast<int>(s.size());
+        string SubStr;
+        for(int i = 0; i < N; ++i)
+        {
+            SubStr += s[i];
+            
+            if(Table.find(SubStr) == Table.end())
+            {
+                Table.insert(SubStr);
+                
+                BackTracking(s, i);
+                
+                Table.erase(SubStr);
+            }
+        }
+        
+        return MaxSplit;
+    }
+    
+private:
+    void BackTracking(const string& S, const int Index)
+    {
+        if(Index + 1 >= N)
+        {
+            MaxSplit = max(MaxSplit, static_cast<int>(Table.size()));
+            return;
+        }
+        
+        string SubStr;
+        for(int i = Index + 1; i < N; ++i)
+        {
+            SubStr += S[i];
+            
+            if(Table.find(SubStr) == Table.end())
+            {
+                Table.insert(SubStr);
+                
+                BackTracking(S, i);
+                
+                Table.erase(SubStr);
+            }
+        }
+    }
+};
