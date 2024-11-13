b0948e7df7188b5e3562d820f1fb2e1fb24bac06
WindOn <62473633+witmaos6@users.noreply.github.com>
2024-11-13 12:39:28 +0900

    Time: 65 ms (57.14%), Space: 60.3 MB (80.83%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper)
+    {
+        sort(nums.begin(), nums.end());
+
+        const int N = static_cast<int>(nums.size());
+        long long Count = 0;
+        
+        for(int i = 0; i < N; ++i)
+        {
+            int FindLower = lower - nums[i];
+            int FindUpper = upper - nums[i];
+            
+            auto Left = lower_bound(nums.begin() + i + 1, nums.end(), FindLower);
+            auto Right = upper_bound(nums.begin() + i + 1, nums.end(), FindUpper);
+            
+            Count += (Right - Left);
+        }
+       
+        return Count;
+    }
+};
