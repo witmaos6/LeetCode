66fb68415c0fc5904a4bcb259cc79d149f1ef977
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-02-19 11:55:04 +0900

    Time: 0 ms (100%), Space: 9.1 MB (75.67%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    string HappyString;
+    int K = 0;
+public:
+    string getHappyString(int n, int k)
+    {
+        if(k > 3 * pow(2, n - 1))
+        {
+            return "";
+        }
+
+        K = k;
+        DFS(0, '-', n);
+        return HappyString;
+    }
+private:
+    bool DFS(int Index, char Prev, const int N)
+    {
+        if(Index == N)
+        {
+            K--;
+            return (K == 0);
+        }
+
+        for(char C = 'a'; C <= 'c'; C++)
+        {
+            if(C != Prev)
+            {
+                if(DFS(Index + 1, C, N))
+                {
+                    HappyString = C + HappyString;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
