0d9f3736c2ed721f410021db642e38812e373a2d
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-03-01 13:44:25 +0900

    Time: 0 ms (100%), Space: 13.1 MB (87.59%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -3,25 +3,30 @@ public:
     vector<int> applyOperations(vector<int>& nums)
     {
         const int N = static_cast<int>(nums.size());
-        vector<int> Result;
-        Result.reserve(N);
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N - 1; i++)
         {
-            if(nums[i] != 0)
+            if(nums[i] == nums[i + 1])
             {
-                if(i + 1 < N && nums[i] == nums[i + 1])
-                {
-                    nums[i] *= 2;
-                    nums[i + 1] = 0;
-                }
-                Result.push_back(nums[i]);
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
         }
 
-        while(Result.size() < N)
+        int i = 0;
+        for(int Num : nums)
         {
-            Result.push_back(0);
+            if(Num != 0)
+            {
+                nums[i] = Num;
+                i++;
+            }
         }
-        return Result;
+
+        while(i < N)
+        {
+            nums[i] = 0;
+            i++;
+        }
+        return nums;
     }
 };
