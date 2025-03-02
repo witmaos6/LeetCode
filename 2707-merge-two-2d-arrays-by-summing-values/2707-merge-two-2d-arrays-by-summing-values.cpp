4ec582f542fcbee9fca8299102da0f74c3e7f139
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-03-02 13:49:58 +0900

    Time: 0 ms (100%), Space: 14.8 MB (44.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -8,37 +8,34 @@ public:
         vector<vector<int>> Result;
         int i = 0;
         int j = 0;
-        while(i < Nums1Size || j < Nums2Size)
+        while(i < Nums1Size && j < Nums2Size)
         {
-            if(i < Nums1Size && j < Nums2Size)
+            if(nums1[i][0] == nums2[j][0])
             {
-                if(nums1[i][0] == nums2[j][0])
-                {
-                    Result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
-                    i++;
-                    j++;
-                }
-                else if(nums1[i][0] < nums2[j][0])
-                {
-                    Result.push_back(nums1[i]);
-                    i++;
-                }
-                else
-                {
-                    Result.push_back(nums2[j]);
-                    j++;
-                }
+                Result.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
             }
-            else if(i < Nums1Size)
+            else if(nums1[i][0] < nums2[j][0])
             {
                 Result.push_back(nums1[i]);
                 i++;
             }
-            else if(j < Nums2Size)
+            else
             {
                 Result.push_back(nums2[j]);
                 j++;
             }
+        }
+        while(i < Nums1Size)
+        {
+            Result.push_back(nums1[i]);
+            i++;
+        }
+        while(j < Nums2Size)
+        {
+            Result.push_back(nums2[j]);
+            j++;
         }
 
         return Result;
