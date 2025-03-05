28c920389ecf6f5b649412a6629fa9ebcc6a97ee
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-03-05 12:58:13 +0900

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -2,16 +2,7 @@ class Solution {
 public:
     long long coloredCells(int n)
     {
-        if(n == 1)
-        {
-            return 1;
-        }
-        long long Result = 1;
-        for(int i = 2; i <= n; i++)
-        {
-            long long AddCell = i;
-            Result += (AddCell * 2) + 2 * (AddCell - 2);
-        }
-        return Result;
+        long long N = n;
+        return 1 + 2 * (N - 1) * N;
     }
 };
