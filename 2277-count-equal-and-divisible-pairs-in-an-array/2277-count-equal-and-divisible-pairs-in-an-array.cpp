3a26cdb09be282547e3a535fd70275c1b6a2a72f
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-04-17 11:54:49 +0900

    Time: 0 ms (100%), Space: 15.5 MB (31.03%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -3,18 +3,16 @@ public:
     int countPairs(vector<int>& nums, int k)
     {
         const int N = static_cast<int>(nums.size());
-        vector<vector<int>> Table(101);
         int Count = 0;
         for(int i = 0; i < N; i++)
         {
-            for(const int& Index : Table[nums[i]]) 
+            for(int j = i + 1; j < N; j++)
             {
-                if((Index * i) % k == 0)
+                if(nums[i] == nums[j] && (i * j) % k == 0)
                 {
                     Count++;
                 }
             }
-            Table[nums[i]].push_back(i);
         }
 
         return Count;
