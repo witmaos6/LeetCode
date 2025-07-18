4091e4c2b0c23737a8d47972b078a4f7f96142c4
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-07-18 11:37:56 +0900

    Time: 71 ms (99.23%), Space: 132 MB (99.48%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+                const int n=nums.size()/3;
+        vector<long long> diff(n+1,0);
+        priority_queue<int> pqL(nums.begin(), nums.begin()+n);
+        priority_queue<int, vector<int>, greater<int>> pqR(nums.begin()+2*n, nums.end());
+        long long sum=accumulate(nums.begin(), nums.begin()+n, 0LL);
+        for(int i=n; i<=2*n; i++){
+            diff[i-n]=sum;
+        
+            int x=nums[i];
+            if (x>=pqL.top()) continue;
+            sum+=x-pqL.top();
+            pqL.pop();
+            pqL.push(x);
+        }
+    
+        sum=accumulate(nums.begin()+2*n, nums.end(), 0LL);
+        for(int i=2*n-1; i>=n-1; i--){
+            diff[i-n+1]-=sum;
+        
+            int x=nums[i];
+            if (x<=pqR.top()) continue;
+            sum+=x-pqR.top();
+            pqR.pop();
+            pqR.push(x);
+        }
+        return *min_element(diff.begin(), diff.end());
+    }
+};
