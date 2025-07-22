118b202e9da9c8d5629e4b3eab1c86d8504e82e2
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-07-22 11:09:43 +0900

    Time: 0 ms (100%), Space: 95.9 MB (96.02%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums)
     {
-        if(nums.size()==1)
-        {
-            return nums[0];
-        }
-        int left = 0;
-        int right = 1;
-        int SumOfSubarray = nums[0];
-        int MaximumSubarray = 0;
-        set<int> Subarray = { nums[0] };
+        const int N = static_cast<int>(nums.size());
+        int L = 0, R = 0;
+        int Maxsum = 0, Sum = 0;
+        vector<int> Table(10001, -1);
 
-        while (right < (int)nums.size())
+        while(R < N)
         {
-            while(Subarray.find(nums[right])!=end(Subarray))
+            if(Table[nums[R]] != -1)
             {
-                Subarray.erase(nums[left]);
-                SumOfSubarray -= nums[left];
-                left++;
+                while(L <= Table[nums[R]])
+                {
+                    Sum -= nums[L];
+                    L++;
+                }
             }
-            Subarray.insert(nums[right]);
-            SumOfSubarray += nums[right];
-            right++;
-            MaximumSubarray = max(MaximumSubarray, SumOfSubarray);
+            Sum += nums[R];
+            Maxsum = max(Maxsum, Sum);
+            Table[nums[R]] = R;
+            R++;
         }
 
-        return MaximumSubarray;
+        return Maxsum;
     }
 };
