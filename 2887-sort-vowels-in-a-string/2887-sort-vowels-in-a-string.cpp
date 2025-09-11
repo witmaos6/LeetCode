effbd22805e8275551f82fddc9205c8fb18692db
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-09-11 12:22:34 +0900

    Time: 9 ms (81.76%), Space: 20.3 MB (11.64%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    array<char, 10> Vowels = {'A','E','I','O','U','a','e','i','o','u'};
+public:
+    string sortVowels(string s)
+    {
+        const int N = static_cast<int>(s.size());
+        array<int, 128> Table = {0};
+        vector<int> SVowelIndex;
+        
+        for(int i = 0; i < N; i++)
+        {
+            if(IsVowel(s[i]))
+            {
+                Table[s[i]]++;
+                SVowelIndex.push_back(i);
+            }
+        }
+        
+        int VowelIndex = 0;
+        
+        for(int Index : SVowelIndex)
+        {
+            while(Table[Vowels[VowelIndex]] <= 0)
+            {
+                VowelIndex++;
+            }
+            
+            s[Index] = Vowels[VowelIndex];
+            Table[Vowels[VowelIndex]]--;
+        }
+        
+        
+        return s;
+    }
+    
+private:
+    bool IsVowel(char C)
+    {
+        for(char Vowel : Vowels)
+        {
+            if(C == Vowel)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
