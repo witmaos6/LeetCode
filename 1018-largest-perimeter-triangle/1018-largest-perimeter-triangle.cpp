9702ab73b50bf0cc1dcfd79b57f22a0571977318
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-09-29 01:28:33 +0900

    Time: 9 ms (34.92%), Space: 25.6 MB (76.13%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -4,16 +4,14 @@ public:
     {
         int N = static_cast<int>(nums.size());
         sort(nums.begin(), nums.end(), greater<>());
-        int MaxPerimeter = 0;
 
         for (int i = 0; i < N - 2; i++)
         {
             if(nums[i] < (nums[i+1] + nums[i+2]))
             {
-                MaxPerimeter = (nums[i] + nums[i + 1] + nums[i + 2]);
-                break;
+                return (nums[i] + nums[i + 1] + nums[i + 2]);
             }
         }
-        return MaxPerimeter;
+        return 0;
     }
 };
