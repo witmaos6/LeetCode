cccc737a553b4581c927e84c278d91df71aed069
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-10-01 11:11:20 +0900

    Time: 0 ms (100%), Space: 7.9 MB (11.04%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -2,20 +2,7 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange)
     {
-        int Drink = 0;
-        int Empty = 0;
-        while(numBottles > 0)
-        {
-            Empty += numBottles;
-            Drink += numBottles;
-            numBottles = 0;
-
-            if(Empty / numExchange >= 1)
-            {
-                numBottles = Empty / numExchange;
-                Empty %= numExchange;
-            }
-        }
-        return Drink;
+        int RemainBottles = (numBottles - 1) / (numExchange - 1);
+        return numBottles + RemainBottles;
     }
 };
