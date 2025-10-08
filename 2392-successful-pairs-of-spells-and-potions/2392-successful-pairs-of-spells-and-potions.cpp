f60b9e0f2ac09a3fd55210490069b9396dea1392
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-10-08 10:34:29 +0900

    Time: 44 ms (53.89%), Space: 124.9 MB (94.62%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success)
     {
+        const int N = static_cast<int>(potions.size());
         sort(potions.begin(), potions.end());
         vector<int> Result;
         Result.reserve(spells.size());
-        const int N = static_cast<int>(potions.size());
 
         for(int& Spell : spells)
         {
-            long long MinSpell = success / Spell;
-            MinSpell += (success % Spell == 0) ? 0 : 1;
+            long long MinSpell = (success + Spell - 1) / Spell;
 
             int LowIndex = lower_bound(potions.begin(), potions.end(), MinSpell) - potions.begin();
             Result.push_back(N - LowIndex);
