719d4dfa263931fb39085b8d415785c577f4c7f6
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-11-04 19:15:26 +0900

    Time: 4 ms (96.81%), Space: 32.7 MB (90.1%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,44 +1,59 @@
 class Solution {
+    using Pair = pair<int, int>;
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x)
     {
         const int N = static_cast<int>(nums.size());
+
+        vector<int> Freq(51);
+        for(int i = 0; i < k; i++)
+        {
+            Freq[nums[i]]++;
+        }
+
+        priority_queue<Pair, vector<Pair>> Heap;
+        for(int i = 0; i <= 50; i++)
+        {
+            if(Freq[i] > 0)
+            {
+                Heap.emplace(Freq[i], i);
+            }
+        }
+
+        int Sum = 0;
+        for(int i = 0; i < x && !Heap.empty(); i++)
+        {
+            Sum += Heap.top().first * Heap.top().second;
+            Heap.pop();
+        }
+
         vector<int> Result;
         Result.reserve(N - k + 1);
+        Result.push_back(Sum);
 
-        for(int i = 0; i <= N - k; i++)
+        for(int i = 1; i <= N - k; i++)
         {
-            vector<int> Sub(nums.begin() + i, nums.begin() + i + k);
-            unordered_map<int, int> Freq;
+            Freq[nums[i - 1]]--;
+            Freq[nums[k + i - 1]]++;
 
-            for(int& Num : Sub)
+            priority_queue<Pair> MaxHeap;
+            for(int i = 0; i <= 50; i++)
             {
-                Freq[Num]++;
-            }
-
-            vector<pair<int, int>> FreqArr(Freq.begin(), Freq.end());
-            sort(FreqArr.begin(), FreqArr.end(), [](auto& A, auto& B){
-                if(A.second == B.second)
-                    return A.first > B.first;
-                return A.second > B.second;
-            });
-
-            unordered_set<int> Top;
-            for(int j = 0; j < FreqArr.size() && j < x; j++)
-            {
-                Top.insert(FreqArr[j].first);
-            }
-
-            int Sum = 0;
-            for(int& Num : Sub)
-            {
-                if(Top.count(Num))
+                if(Freq[i] > 0)
                 {
-                    Sum += Num;
+                    MaxHeap.emplace(Freq[i], i);
                 }
             }
-            Result.push_back(Sum);
+
+            int CurrSum = 0;
+            for(int i = 0; i < x && !MaxHeap.empty(); i++)
+            {
+                CurrSum += MaxHeap.top().first * MaxHeap.top().second;
+                MaxHeap.pop();
+            }
+            Result.push_back(CurrSum);
         }
+        
         return Result;
     }
 };
