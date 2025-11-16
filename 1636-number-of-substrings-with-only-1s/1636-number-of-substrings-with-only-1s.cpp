12e184cd3d91f63c3b51a60e25e7e2efb2673d2e
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-11-16 11:11:47 +0900

    Time: 7 ms (8.48%), Space: 13 MB (5.66%) - LeetHub

---
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSub(string s)
+    {
+        s += '0';
+        const int Mod = 1e9 + 7;
+        int Count1 = 0;
+        int Result = 0;
+
+        for(char& C : s)
+        {
+            if(C == '1')
+            {
+                Count1++;
+            }
+            else if(C == '0')
+            {
+                long long Count = Count1;
+                long long Temp = 1LL * (Count * (Count + 1)) / 2 % Mod;
+                Result += (Temp % Mod);
+                Count1 = 0;
+            }
+        }
+        return Result;
+    }
+};
