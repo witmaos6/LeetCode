c17d4e32ed42e266f3511ed6dc27b6ea1bddac77
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-12-23 11:36:38 +0900

    Time: 82 ms (58.3%), Space: 160.5 MB (75.68%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -2,47 +2,26 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events)
     {
-        sort(events.begin(), events.end(), [](const vector<int>& A, const vector<int>& B) {
-            return A[0] < B[0];
-        });
-        
+        sort(events.begin(), events.end());
+
         const int N = static_cast<int>(events.size());
-        vector<int> SuffixMax(N);
-        SuffixMax[N - 1] = events[N - 1][2];
-        for(int i = N - 2; i >= 0; --i)
+
+        vector<int> Start(N);
+        vector<int> Sufmax(N + 1);
+        for(int i = N - 1; i >= 0; i--)
         {
-            SuffixMax[i] = max(events[i][2], SuffixMax[i + 1]);
+            Start[i] = events[i][0];
+            Sufmax[i] = max(Sufmax[i + 1], events[i][2]);
         }
-        
-        int MaxSum = 0;
+
+        int Result = 0;
         for(int i = 0; i < N; i++)
         {
-            int Left = i + 1;
-            int Right = N - 1;
-            int NextEventIndex = -1;
-            
-            while(Left <= Right)
-            {
-                int Mid = (Right + Left) / 2;
-                if(events[Mid][0] > events[i][1])
-                {
-                    NextEventIndex = Mid;
-                    Right = Mid - 1;
-                }
-                else
-                {
-                    Left = Mid + 1;
-                }
-            }
-            
-            if(NextEventIndex != -1)
-            {
-                int Sum = events[i][2] + SuffixMax[NextEventIndex];
-                MaxSum = max(MaxSum, Sum);
-            }
-            MaxSum = max(MaxSum, events[i][2]);
+            int j = lower_bound(Start.begin(), Start.end(), events[i][1] + 1) - Start.begin();
+            int Memo = events[i][2] + Sufmax[j];
+            Result = max(Result, Memo);
         }
-        
-        return MaxSum;
+
+        return Result;
     }
 };
