e45d7fea058bfd49fcab55bc336ab900dbab2467
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-12-25 12:36:02 +0900

    Time: 130 ms (95.04%), Space: 108 MB (34.4%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -3,18 +3,13 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k)
     {
         sort(happiness.begin(), happiness.end(), greater<>());
-        
-        const int N = static_cast<int>(happiness.size());
+
         long long Result = 0;
-        int Decrease = 0;
         for(int i = 0; i < k; i++)
         {
-            long long Value = happiness[i] - Decrease;
-            Value = (Value > 0) ? Value : 0;
-            Result += Value;
-            Decrease++;
+            long long Num = max(0, happiness[i] - i);
+            Result += Num;
         }
-        
         return Result;
     }
 };
