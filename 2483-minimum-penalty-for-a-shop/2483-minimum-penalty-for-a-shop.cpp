d2c143266cc4b91120c27e45e6aafbebd223e905
WindOn <62473633+witmaos6@users.noreply.github.com>
2025-12-26 11:10:28 +0900

    Time: 0 ms (100%), Space: 13.2 MB (91.48%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -3,22 +3,19 @@ public:
     int bestClosingTime(string customers)
     {
         const int N = static_cast<int>(customers.size());
-        
         int MinPenalty = 0;
         int MinIndex = -1;
-        int PrefixPenalty = 0;
-        
+        int PenaltySum = 0;
+
         for(int i = 0; i < N; i++)
         {
-            PrefixPenalty += (customers[i] == 'Y') ? 1 : -1;
-            
-            if(PrefixPenalty > MinPenalty)
+            PenaltySum += (customers[i] == 'Y') ? 1 : -1;
+            if(PenaltySum > MinPenalty)
             {
-                MinPenalty = PrefixPenalty;
+                MinPenalty = PenaltySum;
                 MinIndex = i;
             }
         }
-        
         return MinIndex + 1;
     }
 };
