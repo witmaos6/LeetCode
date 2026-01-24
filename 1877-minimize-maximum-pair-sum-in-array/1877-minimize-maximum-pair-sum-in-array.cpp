67584f4fa7773e57ed025408e33dbafbcd07edae
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-01-24 12:12:01 +0900

    Time: 174 ms (48.94%), Space: 100 MB (30.18%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    int minPairSum(vector<int>& nums) {
-        int max = INT_MIN;
-        int min = INT_MAX;
-        int hash[100001] = {0};
+    int minPairSum(vector<int>& nums)
+    {
+        const int N = static_cast<int>(nums.size());
+        sort(nums.begin(), nums.end());
+        int Result = INT_MIN;
 
-        for (int num : nums) {
-            hash[num]++;
-            max = std::max(max, num);
-            min = std::min(min, num);
+        for(int i = 0; i < N / 2; i++)
+        {
+            int Sum = nums[i] + nums[N - i - 1];
+            Result = max(Result, Sum);
         }
-
-        int low = min;
-        int high = max;
-        max = INT_MIN;
-        while (low <= high) {
-            if (hash[low] == 0) low++;
-            else if (hash[high] == 0) high--;
-            else {
-                max = std::max(max, low + high);
-                hash[low]--;
-                hash[high]--;
-            }
-        }
-
-        return max;
+        return Result;
     }
 };
