a29f80f1137f96b588a02eb22069ae4eb04c0a64
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-01-31 11:49:54 +0900

    Time: 0 ms (100%), Space: 20 MB (24.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -3,7 +3,6 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target)
     {
         int Index = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
-        
         if(Index < letters.size())
         {
             return letters[Index];
