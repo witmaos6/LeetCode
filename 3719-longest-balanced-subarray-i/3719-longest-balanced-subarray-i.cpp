02764056d3fe2cb9b2abdd4d7987622db368f9db
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-02-10 10:49:58 +0900

    Time: 1959 ms (42.14%), Space: 522.1 MB (50.32%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums)
+    {
+        const int N = static_cast<int>(nums.size());
+        int Result = 0;
+        
+        for(int i = 0; i < N; i++)
+        {
+            unordered_set<int> Table;
+            array<int, 2> Count = {0};
+            for(int j = i; j < N; j++)
+            {
+                if(Table.count(nums[j]) == 0)
+                {
+                    Count[nums[j] & 1]++;
+                    Table.insert(nums[j]);
+                }
+
+                if(Count[0] == Count[1])
+                {
+                    Result = max(Result, j - i + 1);
+                }
+            }
+        }
+        return Result;
+    }
+};
