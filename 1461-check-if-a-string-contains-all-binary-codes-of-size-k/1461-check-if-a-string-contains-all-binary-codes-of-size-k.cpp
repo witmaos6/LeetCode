b3352ae73b81c9bf8fa314169e948924218d85ed
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-02-23 11:58:19 +0900

    Time: 571 ms (25.18%), Space: 150.5 MB (23.37%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k)
+    {
+        const int N = static_cast<int>(s.size());
+        if(N < k)
+            return false;
+
+        unordered_set<string> Table;
+        for(int i = 0; i <= N - k; i++)
+        {
+            string Code = s.substr(i, k);
+            Table.insert(Code);
+        }
+
+        return Table.size() == pow(2, k);
+    }
+};
