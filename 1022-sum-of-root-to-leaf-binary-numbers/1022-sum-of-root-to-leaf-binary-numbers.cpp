86303704b2f2c7d168634037b2ddbe4806087476
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-02-24 12:49:04 +0900

    Time: 0 ms (100%), Space: 17.2 MB (95.36%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -10,62 +10,25 @@
  * };
  */
 class Solution {
-    vector<string> Binaries;
 public:
     int sumRootToLeaf(TreeNode* root)
     {
-        if(!root)
-            return 0;
-
-        DFS(root, "");
-
-        int Sum = 0;
-        for(string& Binary : Binaries)
-        {
-            Sum += BinaryToNum(Binary);
-        }
-        return Sum;
+        return DFS(root, 0);
     }
 
 private:
-    void DFS(TreeNode* Root, string PrevBinary)
+    int DFS(TreeNode* Root, int Curr)
     {
         if(!Root)
-            return;
+            return 0;
 
-        char Curr = ('0' + Root->val);
-        string CurrBinary = PrevBinary + Curr;
+        Curr = Curr * 2 + Root->val;
 
-        if(Root->left || Root->right)
+        if(!Root->left && !Root->right)
         {
-            if(Root->left)
-            {
-                DFS(Root->left, CurrBinary);
-            }
-            if(Root->right)
-            {
-                DFS(Root->right, CurrBinary);
-            }
+            return Curr;
         }
-        else
-        {
-            Binaries.push_back(CurrBinary);
-        }
-    }
 
-    int BinaryToNum(const string& Binary)
-    {
-        const int N = static_cast<int>(Binary.size());
-        int Num = 0;
-        int Pow = N - 1;
-        for(int i = 0; i < N; i++)
-        {
-            if(Binary[i] == '1')
-            {
-                Num += pow(2, Pow);
-            }
-            Pow--;
-        }
-        return Num;
+        return DFS(Root->left, Curr) + DFS(Root->right, Curr);
     }
 };
