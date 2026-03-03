31344ec39c80e645a33895b29a78ba9fe6b2bcb2
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-03-03 19:17:46 +0900

    Time: 0 ms (100%), Space: 7.8 MB (80.15%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -2,33 +2,18 @@ class Solution {
 public:
     char findKthBit(int n, int k)
     {
-        string Bits = "0";
-        for(int i = 0; i < n; i++)
-        {
-            string Post = PostBit(Bits);
-            Bits += '1' + Post;
+        if(n == 1)
+            return '0';
 
-            if(Bits.size() >= k)
-                break;
-        }
-        return Bits[k - 1];
-    }
+        int L = (1 << n) - 1;
+        int Mid = L / 2 + 1;
 
-private:
-    string PostBit(string Bits)
-    {
-        for(char& Bit : Bits)
-        {
-            if(Bit == '0')
-            {
-                Bit = '1';
-            }
-            else if(Bit == '1')
-            {
-                Bit = '0';
-            }
-        }
-        reverse(Bits.begin(), Bits.end());
-        return Bits;
+        if(k == Mid)
+            return '1';
+
+        if(k < Mid)
+            return findKthBit(n - 1, k);
+
+        return findKthBit(n - 1, L - k + 1) == '0' ? '1' : '0';
     }
 };
