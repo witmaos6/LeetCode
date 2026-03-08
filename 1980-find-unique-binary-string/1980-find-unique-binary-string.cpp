decdc0b5189de3f568a3f3c343decdbf32482c4c
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-03-08 12:42:56 +0900

    Time: 0 ms (100%), Space: 12.6 MB (86.66%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums)
     {
-        const int N = static_cast<int>(nums.size());
-        
+        const int N = static_cast<int>(nums[0].size());
         string Result;
-        
         for(int i = 0; i < N; i++)
         {
-            Result += (nums[i][i] == '0' ? '1' : '0');
+            Result += (nums[i][i] == '1') ? '0' : '1';
         }
-        
         return Result;
     }
 };
