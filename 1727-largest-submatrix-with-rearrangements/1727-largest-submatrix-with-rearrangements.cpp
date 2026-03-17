5d899d84142619dfc83dfbde031a7aa5dd76fb36
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-03-17 11:42:13 +0900

    Time: 16 ms (88.83%), Space: 79.7 MB (96.28%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix)
     {
-        const int RowSize = static_cast<int>(matrix.size());
-        const int ColSize = static_cast<int>(matrix[0].size());
+        const int Rows = static_cast<int>(matrix.size());
+        const int Cols = static_cast<int>(matrix[0].size());
         
-        for(int Col = 0; Col < ColSize; Col++)
+        for(int Col = 0; Col < Cols; Col++)
         {
-            for(int Row = 1; Row < RowSize; Row++)
+            for(int Row = 1; Row < Rows; Row++)
             {
                 if(matrix[Row][Col] == 1)
                 {
@@ -17,11 +17,11 @@ public:
         }
         
         int Result = 0;
-        for(int Row = 0; Row < RowSize; Row++)
+        for(int Row = 0; Row < Rows; Row++)
         {
             sort(matrix[Row].begin(), matrix[Row].end(), greater<int>());
             
-            for(int Col = 0; Col < ColSize; Col++)
+            for(int Col = 0; Col < Cols; Col++)
             {
                 int Submatrix = matrix[Row][Col] * (Col + 1);
                 Result = max(Result, Submatrix);
