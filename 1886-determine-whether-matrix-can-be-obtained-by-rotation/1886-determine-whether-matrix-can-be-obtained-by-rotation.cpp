728957a8f29df2450b162cf062139da401b6d40f
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-03-23 01:29:49 +0900

    Time: 0 ms (100%), Space: 14.4 MB (92.37%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -3,72 +3,25 @@ public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target)
     {
         const int N = static_cast<int>(mat.size());
-        if(mat == target)
-            return true;
 
-        bool bPossible = true;
-        int TRow = 0, TCol = 0;
-        for(int Col = 0; Col < N; Col++)
+        for(int i = 0; i < 4; i++)
         {
-            TCol = 0;
-            for(int Row = N - 1; Row >= 0; Row--)
+            if(mat == target)
+                return true;
+
+            for(int i = 0; i < N; i++)
             {
-                if(mat[Row][Col] != target[TRow][TCol])
+                for(int j = i; j < N; j++)
                 {
-                    bPossible = false;
-                    break;
+                    swap(mat[i][j], mat[j][i]);
                 }
-                TCol++;
             }
-            TRow++;
-            if(bPossible == false)
-                break;
-        }
 
-        if(bPossible)
-            return true;
-
-        TRow = 0, TCol = 0;
-        bPossible = true;
-        for(int Row = N - 1; Row >= 0; Row--)
-        {
-            TCol = 0;
-            for(int Col = N - 1; Col >= 0; Col--)
+            for(int i = 0; i < N; i++)
             {
-                if(mat[Row][Col] != target[TRow][TCol])
-                {
-                    bPossible = false;
-                    break;
-                }
-                TCol++;
+                reverse(mat[i].begin(), mat[i].end());
             }
-            if(bPossible == false)
-                break;
-            TRow++;
         }
-
-        if(bPossible)
-            return true;
-
-        bPossible = true;
-        TRow = 0, TCol = 0;
-        for(int Col = N - 1; Col >= 0; Col--)
-        {
-            TCol = 0;
-            for(int Row = 0; Row < N; Row++)
-            {
-                if(mat[Row][Col] != target[TRow][TCol])
-                {
-                    bPossible = false;
-                    break;
-                }
-                TCol++;
-            }
-            TRow++;
-            if(bPossible == false)
-                break;
-        }
-
-        return bPossible;
+        return false;
     }
 };
