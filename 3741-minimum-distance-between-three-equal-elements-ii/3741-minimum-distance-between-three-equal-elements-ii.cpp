f3c106d0ac087074398dc1778fb46d226ae69eef
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-04-11 11:44:56 +0900

    Time: 376 ms (63.5%), Space: 343.2 MB (77.5%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums)
+    {
+        unordered_map<int, vector<int>> Table;
+        const int N = nums.size();
+
+        for(int i = 0; i < N; i++)
+        {
+            Table[nums[i]].push_back(i);
+        }
+
+        int Result = INT_MAX;
+        for(auto&[Num, Indices] : Table)
+        {
+            if(Indices.size() < 3)
+                continue;
+
+            const int Range = Indices.size();
+            for(int i = 0; i <= Range - 3; i++)
+            {
+                int I = Indices[i];
+                int J = Indices[i + 1];
+                int K = Indices[i + 2];
+
+                int Sum = abs(I - J) + abs(J - K) + abs(K - I);
+                Result = min(Result, Sum);
+            }
+        }
+        return (Result == INT_MAX) ? -1 : Result;
+    }
+};
