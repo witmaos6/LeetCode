aac64d6546e1c4a513fc82c5625df4680f6743b8
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-05-03 12:09:58 +0900

    Time: 0 ms (100%), Space: 8.1 MB (61.66%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,42 +1,31 @@
 class Solution {
-    int N = 0;
 public:
     bool rotateString(string s, string goal)
     {
         if(s.size() != goal.size())
             return false;
         
-        N = static_cast<int>(s.size());
-        
-        for(int i = 0; i < N; ++i)
+        const int N = s.size();
+
+        for(int i = 0; i < N; i++)
         {
-            if(RotateString(s, goal, i))
+            bool Flag = true;
+            int Index = 0;
+            int j = i;
+            while(Index < N)
+            {
+                if(s[Index] != goal[j % N])
+                {
+                    Flag = false;
+                    break;
+                }
+                Index++;
+                j++;
+            }
+
+            if(Flag)
                 return true;
         }
-        
         return false;
-    }
-    
-private:
-    bool RotateString(const string& S, const string& Goal, const int Index)
-    {
-        int GIndex = 0;
-        for(int i = Index; i < N; ++i)
-        {
-            if(S[i] != Goal[GIndex])
-                return false;
-            
-            ++GIndex;
-        }
-        
-        for(int i = 0; i < Index; ++i)
-        {
-            if(S[i] != Goal[GIndex])
-                return false;
-            
-            ++GIndex;
-        }
-        
-        return true;
     }
 };
