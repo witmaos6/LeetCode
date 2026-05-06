77bf56ae6a0707035717235982e8fd36c1553dca
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-05-06 10:32:23 +0900

    Time: 16 ms (33.91%), Space: 56.8 MB (69.53%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box)
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid)
     {
-        const int Rows = static_cast<int>(box.size());
-        const int Cols = static_cast<int>(box[0].size());
+        const int Rows = static_cast<int>(boxGrid.size());
+        const int Cols = static_cast<int>(boxGrid[0].size());
         
         vector<vector<char>> RotateBox(Cols, vector<char>(Rows, '.'));
         
         for(int Row = 0; Row < Rows; ++Row)
         {
             int i = Cols - 1;
-            int RotateCol = Rows - Row - 1;
+            const int RotateCol = Rows - Row - 1;
             
             for(int Col = Cols - 1; Col >= 0; --Col)
             {
-                if(box[Row][Col] == '#')
+                if(boxGrid[Row][Col] == '#')
                 {
                     RotateBox[i][RotateCol] = '#';
                     --i;
                 }
-                else if(box[Row][Col] == '*')
+                else if(boxGrid[Row][Col] == '*')
                 {
                     RotateBox[Col][RotateCol] = '*';
                     i = Col - 1;
