5516dfc3f836d2821a7c252594d763911db09e9f
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-05-19 10:52:05 +0900

    Time: 0 ms (100%), Space: 54.4 MB (81.53%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2)
     {
-        const int Num1Size = static_cast<int>(nums1.size());
-        const int Num2Size = static_cast<int>(nums2.size());
+        const int N1 = nums1.size();
+        const int N2 = nums2.size();
         int Index1 = 0;
         int Index2 = 0;
-        
-        while(Index1 < Num1Size && Index2 < Num2Size)
+
+        while(Index1 < N1 && Index2 < N2)
         {
             if(nums1[Index1] == nums2[Index2])
             {
@@ -22,7 +22,6 @@ public:
                 Index1++;
             }
         }
-        
         return -1;
     }
 };
