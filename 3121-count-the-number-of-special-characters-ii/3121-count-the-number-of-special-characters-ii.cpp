85c4278770b230c5315dd37ab5bf5c520496fe64
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-05-27 15:12:21 +0900

    Time: 59 ms (94.66%), Space: 24.3 MB (80.34%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word)
+    {
+        const int N = word.size();
+        vector<pair<int, int>> Table(26, {-1, -1});
+
+        for(int i = 0; i < N; i++)
+        {
+            char C = word[i];
+
+            if(C >= 'a' && C <= 'z')
+            {
+                Table[C - 'a'].first = i;
+            }
+            else if(C >= 'A' && C <= 'Z')
+            {
+                if(Table[C - 'A'].second == -1)
+                {
+                    Table[C - 'A'].second = i;
+                }
+            }
+        }
+
+        int Result = 0;
+        for(auto& [Lower, Upper] : Table)
+        {
+            if(Lower != -1 && Lower < Upper)
+            {
+                Result++;
+            }
+        }
+        return Result;
+    }
+};
