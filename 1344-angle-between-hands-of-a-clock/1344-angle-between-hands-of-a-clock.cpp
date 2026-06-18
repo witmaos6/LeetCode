1f763b8fbcea8f7a74cfc2f39ace7b9f1ce76123
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-06-18 12:00:08 +0900

    Time: 0 ms (100%), Space: 8.2 MB (46.25%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes)
+    {
+        double MinuteAngle = minutes * 6;
+
+        if(hour == 12)
+            hour = 0;
+        double HourAngle = (hour * 30) + (0.5f * minutes);
+
+        double ClockwiseAngle = abs(MinuteAngle - HourAngle);
+        double ReverseClockwiseAngle = 360 - ClockwiseAngle;
+
+        return min(ClockwiseAngle, ReverseClockwiseAngle);
+    }
+};
