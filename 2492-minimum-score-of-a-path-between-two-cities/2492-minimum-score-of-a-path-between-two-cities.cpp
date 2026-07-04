0b91e45d3a081bf403083abe377f8917c8aa6073
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-07-04 10:42:25 +0900

    Time: 52 ms (79.45%), Space: 132.7 MB (73.4%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    typedef pair<int, int> NextCity;
+public:
+    int minScore(int n, vector<vector<int>>& roads)
+    {
+        vector<vector<NextCity>> Graph(n + 1);
+
+        for(vector<int>& City : roads)
+        {
+            Graph[City[0]].push_back({City[1], City[2]});
+            Graph[City[1]].push_back({City[0], City[2]});
+        }
+
+        vector<bool> Visited(n + 1);
+        Visited[1] = true;
+        queue<int> BFS;
+        BFS.push(1);
+        int MinScore = INT_MAX;
+
+        while(!BFS.empty())
+        {
+            int CurrCity = BFS.front();
+            BFS.pop();
+
+            for(auto& [City, Distance] : Graph[CurrCity])
+            {
+                MinScore = min(MinScore, Distance);
+                if(!Visited[City])
+                {
+                    Visited[City] = true;
+                    BFS.push(City);
+                }
+            }
+        }
+
+        return MinScore;
+    }
+};
