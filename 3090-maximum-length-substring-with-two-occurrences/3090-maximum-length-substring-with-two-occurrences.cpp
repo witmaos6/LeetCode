e6ea31fae330fe144d42196b33f63d9d57abf3a7
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-08-14 12:02:50 +0900

    Time: 0 ms (100%), Space: 9.1 MB (77.33%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s)
+    {
+        const int N = s.size();
+
+        vector<int> Table(26);
+        bool TwoOccur = false;
+        int L = 0;
+        int Result = 0;
+        for(int i = 0; i < N; i++)
+        {
+            int Curr = s[i] - 'a';
+            Table[Curr]++;
+
+            while(Table[Curr] > 2)
+            {
+                Table[s[L] - 'a']--;
+                L++;
+            }
+
+            Result = max(Result, i - L + 1);
+        }
+        return Result;
+    }
+};
