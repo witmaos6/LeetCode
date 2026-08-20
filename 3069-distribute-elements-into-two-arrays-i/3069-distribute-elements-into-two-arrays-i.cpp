ce93ed52a6c7c7bac60c9ce13e2591eefb1c933e
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-08-20 12:36:58 +0900

    Time: 3 ms (23.77%), Space: 23.8 MB (66.37%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums)
+    {
+        vector<int> Arr1;
+        vector<int> Arr2;
+
+        Arr1.push_back(nums[0]);
+        Arr2.push_back(nums[1]);
+
+        const int N = nums.size();
+        for(int i = 2; i < N; i++)
+        {
+            if(Arr1.back() > Arr2.back())
+            {
+                Arr1.push_back(nums[i]);
+            }
+            else
+            {
+                Arr2.push_back(nums[i]);
+            }
+        }
+
+        for(int& Num : Arr2)
+        {
+            Arr1.push_back(Num);
+        }
+        return Arr1;
+    }
+};
