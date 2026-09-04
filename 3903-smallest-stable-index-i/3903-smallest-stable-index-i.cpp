f9df391f48fce467fef5adc7b991bdf96732b354
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-04 13:37:27 +0900

    Time: 2 ms (29.25%), Space: 31.6 MB (20.68%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -4,22 +4,23 @@ public:
     {
         const int N = nums.size();
         
-        int Max = nums.front();
-        multiset<int> Mins(nums.begin(), nums.end());
-        int Min = *Mins.begin();
-        if(Max - Min <= k)
-            return 0;
-
+        vector<int> Maxs(N);
+        Maxs[0] = nums[0];
         for(int i = 1; i < N; i++)
         {
-            Max = max(Max, nums[i]);
-            auto It = Mins.find(nums[i - 1]);
-            if(It != Mins.end())
-                Mins.erase(It);
+            Maxs[i] = max(Maxs[i - 1], nums[i]);
+        }
 
-            Min = *Mins.begin();
+        vector<int> Mins(N);
+        Mins[N - 1] = nums[N - 1];
+        for(int i = N - 2; i >= 0; i--)
+        {
+            Mins[i] = min(Mins[i + 1], nums[i]);
+        }
 
-            if(Max - Min <= k)
+        for(int i = 0; i < N; i++)
+        {
+            if(Maxs[i] - Mins[i] <= k)
             {
                 return i;
             }
