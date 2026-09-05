f0056a514da19eac59e1c66ad3d44e4a81f5dd83
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-05 12:56:15 +0900

    Time: 4 ms (89.3%), Space: 202.7 MB (76.32%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -4,13 +4,6 @@ public:
     {
         const int N = nums.size();
 
-        vector<int> Maxs(N);
-        Maxs[0] = nums[0];
-        for(int i = 1; i < N; i++)
-        {
-            Maxs[i] = max(Maxs[i - 1], nums[i]);
-        }
-
         vector<int> Mins(N, INT_MAX);
         Mins[N - 1] = nums[N - 1];
         for(int i = N - 2; i >= 0; i--)
@@ -18,9 +11,11 @@ public:
             Mins[i] = min(Mins[i + 1], nums[i]);
         }
 
+        int Max = nums[0];
         for(int i = 0; i < N; i++)
         {
-            if(Maxs[i] - Mins[i] <= k)
+            Max = max(Max, nums[i]);
+            if(Max - Mins[i] <= k)
                 return i;
         }
         return -1;
