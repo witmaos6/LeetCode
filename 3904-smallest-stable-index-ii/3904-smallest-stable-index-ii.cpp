629d76da71e66dc8066bd852205e68911b0f5e5b
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-05 12:54:54 +0900

    Time: 8 ms (70.04%), Space: 210.9 MB (15.41%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k)
+    {
+        const int N = nums.size();
+
+        vector<int> Maxs(N);
+        Maxs[0] = nums[0];
+        for(int i = 1; i < N; i++)
+        {
+            Maxs[i] = max(Maxs[i - 1], nums[i]);
+        }
+
+        vector<int> Mins(N, INT_MAX);
+        Mins[N - 1] = nums[N - 1];
+        for(int i = N - 2; i >= 0; i--)
+        {
+            Mins[i] = min(Mins[i + 1], nums[i]);
+        }
+
+        for(int i = 0; i < N; i++)
+        {
+            if(Maxs[i] - Mins[i] <= k)
+                return i;
+        }
+        return -1;
+    }
+};
