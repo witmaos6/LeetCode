d4247cc9347ead29c566c3667c8d0cb4e7ae3d72
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-06 11:47:11 +0900

    Time: 7 ms (96.2%), Space: 8.9 MB (91.43%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numDistinct(string s, string t)
+    {
+        if(s.size() < t.size())
+            return 0;
+
+        int SSize = s.size();
+        int TSize = t.size();
+
+        vector<unsigned long long> Memo(TSize + 1);
+        Memo[TSize] = 1;
+
+        for(int i = SSize - 1; i >= 0; i--)
+        {
+            char SC = s[i];
+
+            for(int j = 0; j < TSize; j++)
+            {
+                char TC = t[j];
+                if(SC == TC)
+                {
+                    Memo[j] = Memo[j + 1] + Memo[j];
+                }
+            }
+        }
+        return Memo[0];
+    }
+};
