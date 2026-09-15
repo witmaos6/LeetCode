2ea7b14a8e431207b69df31b57296c0908be3b27
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-15 10:52:56 +0900

    Time: 0 ms (100%), Space: 8.9 MB (91.36%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,66 +1,34 @@
 class Solution {
-    using Pair = pair<int, int>;
 public:
     int maxPalindromes(string s, int k)
     {
         const int N = s.size();
-        vector<Pair> Indices;
-
-        for(int i = 0; i < N; i++)
-        {
-            int Left = i - 1, Right = i + 1;
-            int Count = 1;
-            while(Left >= 0 && Right < N && (s[Left] == s[Right]))
-            {
-                if(Count >= k)
-                {
-                    break;
-                }
-                Count += 2;
-                Left--;
-                Right++;
-            }
-            if(Count >= k)
-            {
-                Indices.push_back({Left + 1, Right - 1});
-            }
-
-            if(i + 1 < N)
-            {
-                Left = i, Right = i + 1;
-                Count = 0;
-                while(Left >= 0 && Right < N && (s[Left] == s[Right]))
-                {
-                    if(Count >= k)
-                    {
-                        break;
-                    }
-                    Count += 2;
-                    Left--;
-                    Right++;
-                }
-                if(Count >= k)
-                {
-                    Indices.push_back({Left + 1, Right - 1});
-                }
-            }
-        }
-
-        sort(Indices.begin(), Indices.end(), [](const Pair& A, const Pair& B){
-            return A.second < B.second;
-        });
-
         int Result = 0;
-        int Prev = -1;
-        for(Pair& P : Indices)
+
+        for(int i = 0; i <= N - k; i++)
         {
-            if(P.first > Prev)
+            if(IsPalindrome(s, i, i + k - 1))
             {
                 Result++;
-                Prev = P.second;
+                i += k - 1;
+            }
+            else if(i + k < N && IsPalindrome(s, i, i + k))
+            {
+                Result++;
+                i += k;
             }
         }
 
         return Result;
+    }
+private:
+    bool IsPalindrome(const string& S, int Left, int Right)
+    {
+        while(Left < Right)
+        {
+            if(S[Left++] != S[Right--])
+                return false;
+        }
+        return true;
     }
 };
