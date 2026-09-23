59aae59005c6c39229c0d197387b969203dfbb87
WindOn <62473633+witmaos6@users.noreply.github.com>
2026-09-23 09:38:17 +0900

    Time: 6 ms (46.25%), Space: 102.3 MB (65.24%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x)
+    {
+        int N = static_cast<int>(nums.size());
+        int Target = accumulate(nums.begin(), nums.end(), 0) - x;
+        
+        if(Target == 0)
+        {
+            return N;
+        }
+        else if(Target < 0)
+        {
+            return -1;
+        }
+        
+        int Sum = 0;
+        int MaxLength = 0;
+        int j = 0;
+        
+        for(int i = 0; i < N; i++)
+        {
+            Sum += nums[i];
+            
+            while (i < N && Sum > Target)
+            {
+                Sum -= nums[j];
+                j++;
+            }
+            
+            if(Sum == Target)
+            {
+                if(i - j + 1 > MaxLength)
+                {
+                    MaxLength = i - j + 1;
+                }
+            }
+        }
+        
+        if(!MaxLength)
+        {
+            return -1;
+        }
+        return N - MaxLength;
+    }
+};
